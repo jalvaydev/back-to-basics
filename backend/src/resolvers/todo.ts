14ba7a0b6970ee85ddaa878d9d3ba0b60a67389b
J Alvarez <jalvay@pop-os.localdomain>
2021-03-08 21:54:32 -0500

    creation and deletion of todos

--- backend/src/resolvers/todo.ts
+++ backend/src/resolvers/todo.ts
@@ -1,6 +1,23 @@
-import { Arg, Ctx, Query, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
 import { Todo } from "@generated/type-graphql";
-import { Context } from "../types";
+import { Context, ErrorResponse } from "../types";
+
+@ObjectType()
+class TodoResponse {
+  @Field(() => Todo, { nullable: true })
+  todo?: Todo;
+
+  @Field(() => ErrorResponse, { nullable: true })
+  errors?: ErrorResponse;
+}
 
 @Resolver()
 class TodoResolver {
@@ -15,6 +32,44 @@ class TodoResolver {
     @Ctx() { prisma }: Context
   ): Promise<Todo | null> {
     return prisma.todo.findUnique({ where: { id } });
+  }
+
+  @Mutation(() => Todo)
+  async createTodo(
+    @Arg("title") title: string,
+    @Arg("body") body: string,
+    @Ctx() { prisma }: Context
+  ): Promise<Todo> {
+    return prisma.todo.create({
+      data: {
+        title,
+        body,
+      },
+    });
+  }
+
+  @Mutation(() => TodoResponse)
+  async deleteTodo(
+    @Arg("id") id: number,
+    @Ctx() { prisma }: Context
+  ): Promise<TodoResponse> {
+    let todo;
+    try {
+      todo = await prisma.todo.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (err) {
+      return {
+        errors: {
+          field: err.code,
+          message: "The todo does not exist, and could not be deleted.",
+        },
+      };
+    }
+
+    return { todo };
   }
 }
 
--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { ObjectType, Field } from "type-graphql";
 
 export type Context = {
   prisma: PrismaClient;
 };
+
+@ObjectType()
+export class ErrorResponse {
+  @Field(() => String)
+  field: string;
+
+  @Field(() => String)
+  message: string;
+}
